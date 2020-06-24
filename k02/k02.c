4002fdf78748c1882d8da72b21403d4d70cdf6cd
bb35319034 <63326020+bb35319034@users.noreply.github.com>
2020-06-24 16:09:14 +0900

    k02 一回目(６月２４日)

--- k02/k02.c
+++ k02/k02.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define _USE_MATH_DEFINES
 #include <math.h>
 
 extern double p_stdnorm(double z);
 
 int main(void)
 {
-    double val;
+    double val, zA, zB, max_val, min_val;
     char fname[FILENAME_MAX];
     char buf[256];
     FILE* fp;
     double L1=1,L2=1;
+    double muA=170.8, muB=169.7, sigmaA=5.43, sigmaB=5.5;
+    
 
     printf("input the filename of sample:");
     fgets(fname,sizeof(fname),stdin);
@@ -26,14 +29,23 @@ int main(void)
 
     while(fgets(buf,sizeof(buf),fp) != NULL){
         sscanf(buf,"%lf",&val);
-
-
-    
-
-
-
+        zA = (val-muA)/sigmaA;
+        zB = (val-muB)/sigmaB;
+        L1 = L1 * p_stdnorm(zA);
+        L2 = L2 * p_stdnorm(zB);
     }
 
+    if(L1<L2)
+    {
+        max_val = L2;
+        min_val = L1;
+    }
+    else
+    {
+        max_val = L1;
+        min_val = L2;
+    }
+    
     if(fclose(fp) == EOF){
         fputs("file close error\n",stderr);
         exit(EXIT_FAILURE);
