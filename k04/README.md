331834ea4c6ecc71703af700c1e4d57f7ea41baa
bb35319034 <63326020+bb35319034@users.noreply.github.com>
2020-07-19 17:02:57 +0900

    k04 提出一回目(７月１９日)

--- k04/README.md
+++ k04/README.md
@@ -1,6 +1,6 @@
 # 課題4 レポート
 
-aa83988848 薗田光太郎
+bb35319034 林健太郎
 
 ## 課題
 
@@ -11,6 +11,7 @@ aa83988848 薗田光太郎
 この二つのファイルを読み取り，学籍番号，身長，性別の3つをメンバーとする構造体の配列で管理するとよい．
 
 ## ソースコードの説明
+まず構造体を宣言しました。ファイルを開いて構造体でまとめて、ファイルを閉じました。IDを入力して身長と性別を表示するのは、配列で考えてそれに対応した身長を出す。性別は１のときはMale,それ以外のときはFemaleと出す。IDがあったときには j に1足すので、j が0のときは No data と出力されるようにする。
 
 
 ## 入出力結果
@@ -37,5 +38,21 @@ Which ID's data do you want? : 45313124
 No data
 ```
 
+
+Input the filename of sample height :c:\Users\admin\Documents\2020psp2\sample\heights.csv
+the filename of sample: c:\Users\admin\Documents\2020psp2\sample\heights.csv
+Input the filename of sample ID :c:\Users\admin\Documents\2020psp2\sample\IDs.csv
+the filename of sample: c:\Users\admin\Documents\2020psp2\sample\IDs.csv
+Which ID's data do you want? : 45313002
+ID : 45313002
+gender : Male
+height : 183.870000
+
+Input the filename of sample height :c:\Users\admin\Documents\2020psp2\sample\heights.csv
+the filename of sample: c:\Users\admin\Documents\2020psp2\sample\heights.csv
+Input the filename of sample ID :c:\Users\admin\Documents\2020psp2\sample\IDs.csv
+the filename of sample: c:\Users\admin\Documents\2020psp2\sample\IDs.csv
+Which ID's data do you want? : 35319034
+No data
 ## 修正履歴
 
--- k04/k04.c
+++ k04/k04.c
@@ -1,1 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+struct human_data
+{
+    int ID;
+    int gender;
+    double height;
+};
+
+
+int main(void)
+{
+    int i=0, j=0, input_ID;
+    char fname[FILENAME_MAX];
+    char buf[256];
+    FILE* fp1;
+    FILE* fp2;
+    struct human_data data[14];
+    
+
+    printf("Input the filename of sample height :");
+    fgets(fname,sizeof(fname),stdin);
+    fname[strlen(fname)-1] = '\0';
+    printf("the filename of sample: %s\n",fname);
+
+    fp1 = fopen(fname,"r");
+    if(fp1==NULL){
+        fputs("File open error\n",stderr);
+        exit(EXIT_FAILURE);
+    }
+
+
+    printf("Input the filename of sample ID :");
+    fgets(fname,sizeof(fname),stdin);
+    fname[strlen(fname)-1] = '\0';
+    printf("the filename of sample: %s\n",fname);
+
+    fp2 = fopen(fname,"r");
+    if(fp2==NULL){
+        fputs("File open error\n",stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    fgets(buf,sizeof(buf),fp1);
+    while(fgets(buf,sizeof(buf),fp1) != NULL){
+        sscanf(buf,"%d, %lf",&data[i].gender, &data[i].height);
+        i++;
+    }
+    i = 0;
+
+    while(fgets(buf,sizeof(buf),fp2) != NULL){
+        sscanf(buf,"%d",&data[i].ID);    
+        i++;
+
+    }
+    if(fclose(fp1) == EOF){
+        fputs("file close error\n",stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    if(fclose(fp2) == EOF){
+        fputs("file close error\n",stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Which ID's data do you want? : ");
+    scanf("%d", &input_ID);
+    for(i=0; i<=13; i++){
+        if(input_ID==data[i].ID){
+            printf("ID : %d\n", data[i].ID);
+            printf("gender : ");
+            if(data[i].gender==1){
+                printf("Male\n");
+            }
+            else{
+                printf("Female\n");
+            }
+            printf("height : %f \n", data[i].height);
+            j++;
+        }
+    }
+    if(j==0){
+        printf("No data\n");
+    }
+
+
+    return 0;
+
+}
+
+
 
